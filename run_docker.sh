846db59085388a0c6c7c2250a6081801adf289df
Antonio Goncalves de Sousa <antonio.goncalvesdesousa@utu.fi>
2023-03-07 16:30:06 +0200

    Change docker image name

--- run_docker.sh
+++ run_docker.sh
@@ -2,5 +2,5 @@
 docker run --rm -ti -e PASSWORD=Totem -p 8787:8787 \
 	-v $PWD/data:/home/rstudio/data -v $PWD/scripts:/home/rstudio/scripts \
 	-v $PWD/notebooks:/home/rstudio/notebooks -v $PWD/results:/home/rstudio/results \
-	scrna-cell-traject 
+	repro-totem-ti.sif 
 
